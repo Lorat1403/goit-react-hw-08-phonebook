51db8a719663ca1b1839d78265c8404b119293a4
Lorat1403 <96732113+Lorat1403@users.noreply.github.com>
2025-04-01 12:03:39 +0000

    Deploying to gh-pages from @ Lorat1403/goit-react-hw-08-phonebook@254d25de0647a799f5725d6fa61bf50ee980187b 🚀

---
+++ static/js/72.629ff912.chunk.js
@@ -0,0 +1,18 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[72],{2683:(e,t,o)=>{o.d(t,{YH:()=>s,i3:()=>i,mc:()=>n});var r=o(2119),c=o(5475);const n=r.Ay.div`
+  padding: ${e=>e.theme.space[0]}px ${e=>e.theme.space[4]}px;
+`,s=r.Ay.h1`
+  padding-top: ${e=>e.theme.space[6]}px;
+  width: 100%;
+  font-size: ${e=>e.theme.fontSizes.l};
+  margin-left: ${e=>e.theme.space[9]};
+  margin-right: ${e=>e.theme.space[9]};
+  align-item: center;
+`,i=(0,r.Ay)(c.N_)`
+  color: ${e=>e.theme.colors.orangered};
+  transition: ${e=>e.theme.transition.color};
+  :hover,
+  :focus {
+    color: ${e=>e.theme.colors.white};
+  }
+`},5072:(e,t,o)=>{o.r(t),o.d(t,{default:()=>i});var r=o(5137),c=o(1970),n=o(2683),s=o(579);const i=()=>{const e=(0,r.d4)(c.Pg.getIsLoggedIn);return(0,s.jsx)("section",{children:(0,s.jsx)(n.mc,{children:e?(0,s.jsx)(n.YH,{children:"Thank you! Now you can add your contacts!"}):(0,s.jsxs)(n.YH,{children:["Welcome, dear Guest! Please,"," ",(0,s.jsx)(n.i3,{to:"register",children:"Sign up"})," or"," ",(0,s.jsx)(n.i3,{to:"login",children:"Log in"}),"."]})})})}}}]);
+//# sourceMappingURL=72.629ff912.chunk.js.map
---
+++ static/js/92.1a263ecd.chunk.js
@@ -0,0 +1,15 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[92],{7119:(e,t,n)=>{n.d(t,{L:()=>o});var a=n(5475),r=n(2119);r.Ay.form`
+  width: 400px;
+  display: flex;
+  flex-direction: column;
+  margin-left: ${e=>e.theme.space[9]};
+  margin-right: ${e=>e.theme.space[9]};
+`;const o=(0,r.Ay)(a.N_)`
+  font-family: 'Arial';
+  transition: ${e=>e.theme.transition.color};
+  :hover,
+  :focus {
+    color: ${e=>e.theme.colors.blue};
+  }
+`},9092:(e,t,n)=>{n.r(t),n.d(t,{default:()=>y});var a=n(5043),r=n(5137),o=n(8862),s=n(8567),i=n(3849),l=n(7732),c=n(9353),m=n(8436),d=n(3960),u=n(5426),h=n(7812),p=n(4757),x=n(6325),g=n(7119),f=n(1970),A=n(579);const j=(0,o.A)();function y(){const e=(0,r.wA)(),[t,n]=(0,a.useState)(""),[o,y]=(0,a.useState)(""),b=e=>{let{target:{name:t,value:a}}=e;switch(t){case"email":return n(a);case"password":return y(a);default:return}};return(0,A.jsx)(s.A,{theme:j,children:(0,A.jsxs)(i.Ay,{container:!0,component:"div",sx:{height:"100vh"},children:[(0,A.jsx)(l.Ay,{}),(0,A.jsx)(i.Ay,{item:!0,xs:!1,sm:4,md:7,sx:{backgroundImage:"url(https://source.unsplash.com/random)",backgroundRepeat:"no-repeat",backgroundColor:e=>"light"===e.palette.mode?"#fcfc81":e.palette.grey[900],backgroundSize:"cover",backgroundPosition:"center"}}),(0,A.jsx)(i.Ay,{item:!0,xs:12,sm:8,md:5,component:c.A,elevation:6,sx:{backgroundColor:"#fcfc81"},square:!0,children:(0,A.jsxs)(m.A,{sx:{my:8,mx:4,display:"flex",flexDirection:"column",alignItems:"center"},children:[(0,A.jsx)(d.A,{sx:{m:1,bgcolor:"#969287"},children:(0,A.jsx)(x.A,{})}),(0,A.jsx)(u.A,{component:"h1",variant:"h5",children:"Log in"}),(0,A.jsxs)(m.A,{component:"form",noValidate:!0,onSubmit:async a=>{a.preventDefault(),await e(f.Th.logIn({email:t,password:o})),n(""),y("")},sx:{mt:1},children:[(0,A.jsx)(h.A,{margin:"normal",required:!0,fullWidth:!0,id:"email",label:"Email Address",name:"email",autoComplete:"email",autoFocus:!0,onChange:b}),(0,A.jsx)(h.A,{margin:"normal",required:!0,fullWidth:!0,name:"password",label:"Password",type:"password",id:"password",autoComplete:"current-password",onChange:b}),(0,A.jsx)(p.A,{type:"submit",fullWidth:!0,variant:"contained",sx:{mt:3,mb:2},disabled:""===t||""===o,children:"Log In"}),(0,A.jsx)(i.Ay,{container:!0,justifyContent:"center",children:(0,A.jsx)(i.Ay,{item:!0,children:(0,A.jsx)(g.L,{to:"/register",children:"Don't have an account? Sign Up"})})})]})]})})]})})}}}]);
+//# sourceMappingURL=92.1a263ecd.chunk.js.map
